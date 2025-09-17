0cb15df61aa5efcced07e64699cb0ae9266d47bb
Paulo Vilela <paulov4@hotmail.com>
2025-09-17 19:29:50 -0300

    chore: initial commit - Atlântica Hotel

---
+++ src/main/java/com/example/atlantica_hotel/AtlanticaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.atlantica_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlanticaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlanticaHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atlantica_hotel/AtlanticaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atlantica_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlanticaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
